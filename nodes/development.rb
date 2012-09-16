b1071de56832d5812055e1e5d23d89fb27cf8f85
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-16 02:06:13 -0700

    Revert "alrady in release dir, so pwd suffices"
    This reverts commit 4d3f578dcf57d0d6659be34cc1d48437b205adfb.

--- nodes/development.rb
+++ nodes/development.rb
@@ -2,7 +2,7 @@ run_list []
 platform "generic"
 platform_version "0.0.1"
 
-release_dir ENV['PWD']
-project_dir ENV['PWD']
+release_dir ENV['PWD'].sub('definitions', 'vagrant')
+project_dir ENV['PWD']('definitions', 'vagrant')
 home_dir ENV['HOME']
 app_env "development"
