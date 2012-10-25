51be3c067dc731662b5acdee6b992a90fdc8b1ae
Tom Bombadil <amanibhavam@destructuring.org>
2012-10-24 23:59:30 -0700

    debug where guest additions are mounted

--- vagrant.sh
+++ vagrant.sh
@@ -9,15 +9,8 @@ aptitude update
 aptitude safe-upgrade -q -y
 
 # vbox guest additions
-#ver_virtualbox="$(cat .vbox_version)"
-ver_virtualbox="4.1.22"
-url_guestadditions="http://download.virtualbox.org/virtualbox/$ver_virtualbox/VBoxGuestAdditions_$ver_virtualbox.iso"
-pth_guestadditions="$HOME/VBoxGuestAdditions_$ver_virtualbox.iso"
-wget -nv -O "$pth_guestadditions" "$url_guestadditions"
-mount -o loop "$pth_guestadditions" /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm -f "$pth_guestadditions"
+df
+mount
 
 # wake up eth0
 perl -pe 'm{exit 0} && print "dhclient eth0\n"' -i /etc/rc.local
